1eb206dfcd0782a83291785fe166cccaaff98cc6
Adam Elimelech <adamelimelech@Adams-MacBook-Pro.local>
2016-12-03 00:45:22 +0200

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,26 @@
+var axios = require('axios');
+
+const openWeatherMapUrl = 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=c755c22091a5eaeff5fd2f711b6b1fd9';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${openWeatherMapUrl}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function(res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+     },
+     function(res) {
+       if( res.data){
+         throw new Error(res.data.message);
+       } else {
+         throw new Error(res || "Cannot find city");
+       }
+
+    });
+  }
+}
---
+++ app/components/Examples.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+
+var Exapmles = (props) => {
+  return(
+    <h3>Examples component</h3>
+  )
+};
+module.exports = Exapmles;
